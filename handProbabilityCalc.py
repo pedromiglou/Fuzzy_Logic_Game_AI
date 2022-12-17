303108b8a9a6c679a430b7eda53d62e15a316ce1
Francisco Marques <franciscocmarques@ua.pt>
2022-12-17 21:40:11 +0000

    update

--- handProbabilityCalc.py
+++ handProbabilityCalc.py
@@ -1,12 +1,5 @@
 from treys import Card, Deck, Evaluator
 
-# royal flush combinations as treys doesnt discriminate royal flush from straight flush
-royal_flush= [[Card.new('Ts'),Card.new('Js'),Card.new('Qs'),Card.new('Ks'),Card.new('As')],
-              [Card.new('Td'),Card.new('Jd'),Card.new('Qd'),Card.new('Kd'),Card.new('Ad')],
-              [Card.new('Tc'),Card.new('Jc'),Card.new('Qc'),Card.new('Kc'),Card.new('Ac')],
-              [Card.new('Th'),Card.new('Jh'),Card.new('Qh'),Card.new('Kh'),Card.new('Ah')]]
-
-
 # initializes Deck and Evaluator
 deck = Deck()
 evaluator = Evaluator()
@@ -24,35 +17,29 @@ p_hand = deck.draw(n_player)
 deck = deck.draw(n) # draws remaining cards so its easier to access them
 
 #prints current cards and cards available in the deck
-Card.print_pretty_cards(board+p_hand)
-Card.print_pretty_cards(deck)
+print("Player's hand: ")
+Card.print_pretty_cards(p_hand)
+print("Community cards: ")
+Card.print_pretty_cards(board)
 print()
 
 
 # computes ocurrences with only 4 cards in the board, 2nd turn
-prob_2nd_turn = {}
+prob_turn = {}
 for i in range(n):
     # draws a card to the board
     board_sim = board + [deck[i]]
     p_score_sim = evaluator.evaluate(board_sim, p_hand)
     p_class_sim = evaluator.class_to_string(evaluator.get_rank_class(p_score_sim))
+       
+    try:
+        prob_turn[p_class_sim] += 1
     
-    # verifies if the hand is a royal flush or not
-    if sorted(board_sim + p_hand) in sorted(royal_flush):
-        try:
-            prob_2nd_turn['Royal Flush'] += 1
-    
-        except:
-            prob_2nd_turn['Royal Flush'] = 1
-    else:      
-        try:
-            prob_2nd_turn[p_class_sim] += 1
-        
-        except:
-            prob_2nd_turn[p_class_sim] = 1
+    except:
+        prob_turn[p_class_sim] = 1
 
-# computes ocurrences with 5 cards in the board, last turn
-prob_3rd_turn = {}
+# computes ocurrences with 4 and then 5 cards in the board, last turn
+prob_turn_and_river = {}
 for i in range(n):
     # draws a card to the board
     board_sim = board + [deck[i]]
@@ -61,24 +48,31 @@ for i in range(n):
         board_sim = board + [deck[j]]
         p_score_sim = evaluator.evaluate(board_sim, p_hand) 
         p_class_sim = evaluator.class_to_string(evaluator.get_rank_class(p_score_sim)) 
-        
-        # verifies if the hand is a royal flush or not
-        if sorted(board_sim + p_hand) in sorted(royal_flush):
-            try:
-                prob_2nd_turn['Royal Flush'] += 1
-        
-            except:
-                prob_2nd_turn['Royal Flush'] = 1
-        else:
-            try:
-                prob_3rd_turn[p_class_sim] += 1
-        
-            except:
-                prob_3rd_turn[p_class_sim] = 1
+
+        try:
+            prob_turn_and_river[p_class_sim] += 1
+    
+        except:
+            prob_turn_and_river[p_class_sim] = 1
+
+board = board + [deck.pop()] # turns the 4th card
+prob_river = {}
+# computes ocurrences with 5 cards in the board, last turn
+for i in range(n-1):
+    board_sim = board + [deck[i]]
+    p_score_sim = evaluator.evaluate(board_sim, p_hand) 
+    p_class_sim = evaluator.class_to_string(evaluator.get_rank_class(p_score_sim)) 
+    try:
+        prob_river[p_class_sim] += 1
+
+    except:
+        prob_river[p_class_sim] = 1
+
 
 # calculates probability for the hands
-prob_2nd_turn.update((key, value /n) for key, value in prob_2nd_turn.items())
-prob_3rd_turn.update((key, value /(n*(n-1))) for key, value in prob_3rd_turn.items())
+prob_turn.update((key, value /n) for key, value in prob_turn.items())
+prob_turn_and_river.update((key, value /(n*(n-1))) for key, value in prob_turn_and_river.items())
+prob_river.update((key, value /(n-1)) for key, value in prob_river.items())
 
 print(r"Hands that are not displayed have 0% chance of ocurring.")
 print()
@@ -86,11 +80,19 @@ print()
 
 print("2nd turn (Turn)")
 print("Hand\tProbability (%)")
-for k,v in prob_2nd_turn.items():
+for k,v in prob_turn.items():
     print(k,v)
 print()
 
-print("3rd/Last turn (River)")
+print("2nd and 3rd turn (Turn and River)")
 print("Hand\tProbability (%)")
-for k,v in prob_3rd_turn.items():
+
+for k,v in prob_turn_and_river.items():
+    print(k,v)
+print()
+
+print("3rd turn (River) after drawing the 4th card")
+print("Hand\tProbability (%)")
+
+for k,v in prob_river.items():
     print(k,v)
